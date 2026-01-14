023701113fd5ab606b54674aaac1d1852bc9926c
Eric Flick <flick.ej@gmail.com>
2026-01-14 14:11:55 +0800

    Push piece if it can almost rotate. Still need to make this better as it's hard to get into spaces on the side. Especially for I-beam

--- player.c
+++ player.c
@@ -1,6 +1,6 @@
 #include "player.h"
-#include "shapes.h"
 #include "board.h"
+#include "shapes.h"
 
 static Player player;
 static Shape shapeQueue[TOTAL_SHAPES];
@@ -12,6 +12,7 @@ static void ShuffleQueue() {
     shapeQueue[i] = i;
   }
 
+  // Randomly swap elements.
   for (int i = 0; i < TOTAL_SHAPES; i++) {
     Sint32 swapWith = SDL_rand(TOTAL_SHAPES);
     Shape tmp = shapeQueue[i];
@@ -24,7 +25,12 @@ static void ShuffleQueue() {
 
 void InitPlayer() {
   ShuffleQueue();
-  player = (Player){.shape = queuePos, .rotation = 0, .x = 0, .y = 0, .lastFall = 0};
+  player = (Player){.shape = queuePos,
+                    .rotation = 0,
+                    .x = 0,
+                    .y = 0,
+                    .lastFall = 0,
+                    .rotateGracePeriod = 0};
 }
 
 static void NextShape() {
@@ -39,6 +45,7 @@ static void NextShape() {
   player.shape = queuePos;
   player.lastFall = 0;
   player.rotation = 0;
+  player.rotateGracePeriod = 0;
 }
 
 /// Moves the player down one space.
@@ -49,17 +56,23 @@ static bool MoveDown() {
     return true;
   }
 
-  OccupyBoardCells(player.x, player.y, player.shape, player.rotation, ShapeColor(player.shape));
-  NextShape();
+  if (player.rotateGracePeriod == 0) {
+    OccupyBoardCells(player.x, player.y, player.shape, player.rotation,
+                    ShapeColor(player.shape));
+    NextShape();
+  } else {
+    player.rotateGracePeriod--;
+  }
   return false;
 }
 
 /// Immediately drops the piece as low as possible.
 static void Teleport() {
-  while (MoveDown()) {}
+  while (MoveDown()) {
+  }
 }
 
-void TickPlayer() { 
+void TickPlayer() {
   if (player.lastFall == 30) {
     MoveDown();
     player.lastFall = 0;
@@ -68,42 +81,52 @@ void TickPlayer() {
   player.lastFall++;
 }
 
-void DrawPlayer(SDL_Renderer *renderer) {
+void DrawPlayer(SDL_Renderer* renderer) {
   for (int y = 0; y < 4; y++) {
     for (int x = 0; x < 4; x++) {
-      if (ShapeCellAt(player.shape, player.rotation, x, y) != SHAPE_EMPTY_CELL) {
-        DrawCell(renderer, x + player.x, y + player.y, ShapeColor(player.shape));
+      if (ShapeCellAt(player.shape, player.rotation, x, y) !=
+          SHAPE_EMPTY_CELL) {
+        DrawCell(renderer, x + player.x, y + player.y,
+                 ShapeColor(player.shape));
       }
     }
   }
 }
 
 void HandlePlayerInput(SDL_Event event) {
-    switch (event.key.scancode) {
-        case SDL_SCANCODE_X: {
-          int nextRotation = (player.rotation + 1) % 4;
-          if (CanPlaceShapeAt(player.x, player.y, player.shape, nextRotation))
-            player.rotation = nextRotation;
-        } break;
-
-        case SDL_SCANCODE_LEFT: {
-          if (CanPlaceShapeAt(player.x - 1, player.y, player.shape, player.rotation))
-            player.x -= 1;
-        } break;
-
-        case SDL_SCANCODE_RIGHT: {
-          if (CanPlaceShapeAt(player.x + 1, player.y, player.shape, player.rotation))
-            player.x += 1;
-        } break;
-
-        case SDL_SCANCODE_DOWN: {
-          player.lastFall = 30;
-        } break;
-
-        case SDL_SCANCODE_UP: {
-          Teleport();
-        } break;
-
-        default: break;
+  switch (event.key.scancode) {
+  case SDL_SCANCODE_X: {
+    int nextRotation = (player.rotation + 1) % 4;
+    if (CanPlaceShapeAt(player.x, player.y, player.shape, nextRotation)) {
+      player.rotation = nextRotation;
+    } else if (CanPlaceShapeAt(player.x - 1, player.y, player.shape, nextRotation)) {
+      player.x -= 1;
+      player.rotation = nextRotation;
+    } else if (CanPlaceShapeAt(player.x + 1, player.y, player.shape, nextRotation)) {
+      player.x += 1;
+      player.rotation = nextRotation;
     }
+  } break;
+
+  case SDL_SCANCODE_LEFT: {
+    if (CanPlaceShapeAt(player.x - 1, player.y, player.shape, player.rotation))
+      player.x -= 1;
+  } break;
+
+  case SDL_SCANCODE_RIGHT: {
+    if (CanPlaceShapeAt(player.x + 1, player.y, player.shape, player.rotation))
+      player.x += 1;
+  } break;
+
+  case SDL_SCANCODE_DOWN: {
+    player.lastFall = 30;
+  } break;
+
+  case SDL_SCANCODE_UP: {
+    Teleport();
+  } break;
+
+  default:
+    break;
+  }
 }
--- player.h
+++ player.h
@@ -10,6 +10,7 @@ typedef struct {
   int x;
   int y;
   int lastFall;
+  int rotateGracePeriod;
   bool rotationPressed;
 } Player;
 
